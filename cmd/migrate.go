90be79870dd4110dde8c758c3ba53baa6fc8fee9
arcranion <devlaq2331@gmail.com>
2024-08-26 22:34:25 +0900

    fix: serve migrate now load .env

--- cmd/migrate.go
+++ cmd/migrate.go
@@ -6,9 +6,11 @@ import (
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/lib/pq"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"log"
 	"os"
+	"surge/internal/conf"
 	. "surge/internal/utilities"
 )
 
@@ -28,7 +30,13 @@ func buildMigrateCommand() *cobra.Command {
 }
 
 func handleMigrateCommand(cmd *cobra.Command, args []string) error {
-	databaseUrl := os.Getenv("SURGE_DATABASE_URL")
+	config, err := conf.LoadFromEnvironments()
+	if err != nil {
+		logrus.Errorf("Failed to load configuration from environments")
+		return err
+	}
+
+	databaseUrl := config.Database.Url
 	sourceUrl := StringDefault(os.Getenv("SURGE_MIGRATIONS_PATH_OVERRIDE"), "file://./migrations")
 
 	log.Printf("%s -> %s", sourceUrl, databaseUrl)
